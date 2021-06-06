acdd3bcda4f61fe87a76c9074c2aea866988536e
Victor Rodriguez Sanchez <victor.rodriguez.sanchez@alumnos.upm.es>
2021-06-06 11:56:03 +0200

    Create Interaction.h

---
+++ Tanques/src/Interaction.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "tank.h"
+#include "wall.h"
+#include "vector3D.h"
+#include "Disparo.h"
+#include "Enemigo.h"
+
+class Interaction
+{
+	public:
+	static void colision(tank& Tank, Wall wall);
+	static void colision(Enemigo& Tank, Wall wall);
+	static bool colision(tank Tank, Disparo disparo);
+	static bool colision(Enemigo Tank, Disparo disparo);
+
+	static void choque(tank& tank1, Enemigo& tank2);
+	static bool colision(Disparo& disparo, Wall& wall);
+
+};
+
