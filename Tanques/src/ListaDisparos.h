8328585a4dab49714b24fb926bf9b36dbf74e970
Victor Rodriguez Sanchez <victor.rodriguez.sanchez@alumnos.upm.es>
2021-06-06 11:55:59 +0200

    Create ListaDisparos.h

---
+++ Tanques/src/ListaDisparos.h
@@ -0,0 +1,34 @@
+#pragma once
+#define MAX_DISPAROS 3
+#include "Interaction.h"
+#include "Disparo.h"
+#include "tank.h"
+#include "Enemigo.h"
+#include "Wall.h"
+#include "WallList.h"
+
+class ListaDisparos
+{
+	
+public:
+	ListaDisparos();
+	virtual ~ListaDisparos();
+	bool add(Disparo*);
+	void draw();
+	void destruirContenido();
+	void mueve(float t);
+	void eliminar(int index); // la borra segun su indice
+	void eliminar(Disparo* e); // elimina una esfera segun su direccion de memoria
+
+	Disparo* operator[](int i);
+	Disparo* colision(tank& Tank);
+	Disparo* colision(Enemigo& Tank);
+	//Disparo* contador(Disparo*d);
+
+	int getNumero() { return numero; }
+private:
+	int numero;
+	Disparo* list[MAX_DISPAROS];
+	
+};
+
