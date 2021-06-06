1acbcf0077c1aa5420ad758a5657e9125dda362b
Victor Rodriguez Sanchez <victor.rodriguez.sanchez@alumnos.upm.es>
2021-06-06 11:55:28 +0200

    Create mundo.h

---
+++ Tanques/src/mundo.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "tank.h"
+#include "inputstates.h"
+#include "camera.h"
+#include "WallList.h"
+#include "Interaction.h"
+#include "Disparo.h"
+#include "Enemigo.h"
+#include "ListaDisparos.h"
+
+class mundo{
+	tank tanque;
+	inputstates estadosentradas;
+	camera camara;
+	WallList paredes;
+	Interaction interaccion;
+	Disparo disparo;
+	Enemigo enemigo;
+	ListaDisparos disparos;
+public:
+	mundo();
+	virtual ~mundo();
+	void refresh();
+	void draw();
+	void key(unsigned char key, int x, int y,bool state);
+	void tecla();
+	void raton();
+	void mousemotion(int x, int y);
+	void mouseclick(int button,int state);
+};
